7ea5bc7c80386e0001e7c98e9783da443597cefe
Robert <robert.harkonen@helsinki.fi>
2018-04-07 15:30:25 +0300

    Fraktaalilogiikan perusominaisuudet valmiita, UI-luokka luotu

--- ChaosGame/src/main/java/chaosgame/domain/Fractal.java
+++ ChaosGame/src/main/java/chaosgame/domain/Fractal.java
@@ -9,16 +9,37 @@ public class Fractal {
     
     private Settings settings;
     private Node[][] grid;
-    private float currentX;
-    private float currentY;
+    private Node current;
     
     public Fractal(int width, int height) {
+        this.settings = new Settings(0.5);
         this.grid = new Node[width][height];
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
                 grid[i][j] = new Node(i, j, Nodetype.EMPTY);
             }
         }
+        
+        this.current = grid[0][0];
+    }
+    
+    public Fractal(int width, int height, Settings set) {
+        this.settings = set;
+        this.grid = new Node[width][height];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                grid[i][j] = new Node(i, j, Nodetype.EMPTY);
+            }
+        }
+        
+        this.current = settings.getFirstAnchor();       //empty node (0,0) if not found
+    }
+    
+    public void addAnchor(int x, int y) {
+        settings.addAnchor(x, y);
+        setType(x, y, Nodetype.ANCHOR);
+        
+        current = settings.getFirstAnchor();        //starting from an anchor makes the drawing cleaner
     }
     
     public void setType(int x, int y, Nodetype type) {
@@ -30,6 +51,22 @@ public class Fractal {
             for (int j = 0; j < grid[0].length; j++) {
                 setType(i, j, type);
             }
+        }
+    }
+    
+    /** Moves current node toward the next randomly chosen anchor
+     * by the current ratio.
+     */
+    public void iterate() {
+        Node nextAnchor = settings.getRandomAnchor();
+        long nextX = Math.round(current.getX() 
+                + settings.getRatio()*(nextAnchor.getX() - current.getX()));
+        long nextY = Math.round(current.getY() 
+                + settings.getRatio()*(nextAnchor.getY() - current.getY()));
+        current = grid[(int) nextX][(int) nextY];               //coordinates are always within int bounds
+        
+        if (current.getType() == Nodetype.EMPTY) {
+            current.setType(Nodetype.FILLED);
         }
     }
 }
--- ChaosGame/src/main/java/chaosgame/domain/Settings.java
+++ ChaosGame/src/main/java/chaosgame/domain/Settings.java
@@ -6,7 +6,52 @@
 
 package chaosgame.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Settings {
+    private List<Node> anchors;
+    private double ratio;
+    private Random rand;
     
+    public Settings(double rat) {
+        this.anchors = new ArrayList<>();
+        this.ratio = rat;
+        this.rand = new Random();
+    }
+    
+    public void addAnchor(int x, int y) {
+        anchors.add(new Node(x, y, Nodetype.ANCHOR));
+    }
+    
+    public void removeAnchors() {
+        anchors = new ArrayList<>();
+    }
+    
+    public double getRatio() {
+        return this.ratio;
+    }
+    
+    public void setRatio(double rat) {
+        this.ratio = rat;
+    }
+    
+    public Node getFirstAnchor() {
+        if (anchors.isEmpty()) {
+            return new Node(0, 0, Nodetype.EMPTY);
+        }
+        return anchors.get(0);
+    }
+    
+    public List<Node> getAnchors() {
+        return this.anchors;
+    }
+    
+    public Node getRandomAnchor() {
+        if (anchors.isEmpty()) {
+            return new Node(0, 0, Nodetype.EMPTY);
+        }
+        return anchors.get(rand.nextInt(anchors.size()));
+    }
 }
